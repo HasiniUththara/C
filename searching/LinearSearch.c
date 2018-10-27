856188020ca60a60dc657c35decbc8880f1d7805
PubuduPraneeth <pubudupraneeth21@gmail.com>
2018-10-27 09:43:55 +0530

    i added lenear search

--- searching/LinearSearch.c
+++ searching/LinearSearch.c
@@ -1,27 +1,26 @@
 #include <stdio.h>
 
-int linearsearch(int *arr, int size, int val){
-	int i;
-	for (i = 0; i < size; i++){
-		if (arr[i] == val)
-			return 1;
-	}
-	return 0;
-}
+int main()
+{
+    int arr[6]={5,9,12,3,36,28};
+    printf("\nEnter the key to search : ");
+    int num,x,found;
+    scanf("%d",&num);
+    for(x=0;x<6;x++)
+    {
+        if(num ==arr[x]  )
+        {
+            printf("\nkey has found in %d index",x);
+	    found=1;
+	    break;
+        }
+       else{continue;}
 
-void main(){
-	int s,i,v;
-	printf("Enter the size of the array:\n");
-	scanf("%d",&s);
+    }
 
-	int a[s];
-	printf("Enter the contents for an array of size %d:\n", s);
-	for (i = 0; i < s; i++)	scanf("%d", &a[i]);
-
-	printf("Enter the value to be searched:\n");
-	scanf("%d", &v);
-	if (linearsearch(a, s, v))
-		printf("Value %d is in the array.\n", v);
-	else
-		printf("Value %d is not in the array.\n", v);
+    if(found!=1)
+    {
+        printf("Search not found!");
+    }
+    return 0;
 }
